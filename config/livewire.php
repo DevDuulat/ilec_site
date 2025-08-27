c04a30e595952ccea477f826df880e3ef8c6e4fe
DevDuulat <duulat.bekm@mail.ru>
2025-08-27 13:45:47 +0600

    fix

--- config/livewire.php
+++ config/livewire.php
@@ -3,16 +3,16 @@
 return [
 
     'temporary_file_upload' => [
-        'disk' => null,
-        'rules' => 'required|file|max:512000', // 500MB
-        'directory' => null,
-        'middleware' => null,
+        'disk' => local, // или 'local', 's3'
+        'rules' => 'file|max:512000', // 500MB
+        'directory' => 'livewire-tmp',
+        'middleware' => 'throttle:60,1',
         'preview_mimes' => [
             'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
             'mov', 'avi', 'wmv', 'mp3', 'm4a',
             'jpg', 'jpeg', 'mpga', 'webp', 'wma',
         ],
-        'max_upload_time' => 5,
+        'max_upload_time' => 10, // минуты
     ],
 
 
