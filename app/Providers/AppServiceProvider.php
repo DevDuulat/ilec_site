be1357d1094cff0b011d280fdced9ab78851d847
DevDuulat <duulat.bekm@mail.ru>
2025-08-27 13:34:09 +0600

    fix fileupload

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,8 +20,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Livewire::listen('file.upload:validation', function ($validator) {
-            $validator->setRules(['file' => 'file|max:512000']); // 500 MB в KB
-        });
     }
 }
